e232719088eab3a79513b8447e4488ee4d9678a8
m-rejdych <rejdych.m.r@gmail.com>
2024-07-30 00:07:35 +0200

    Updated node-starter template.

--- templates/node-starter/src/index.ts
+++ templates/node-starter/src/index.ts
@@ -4,10 +4,10 @@ import { config } from 'dotenv';
 config();
 
 const HOST = process.env.HOST ?? 'localhost';
-const PORT = process.env.PORT ?? 8080;
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
 const app = express();
 
 app.get('/hello', (_, res) => res.json({ hi: 'hello!' }));
 
-app.listen(PORT, () => console.log(`Listening on http://${HOST}:${PORT}`));
+app.listen(PORT, HOST, () => console.log(`Listening on http://${HOST}:${PORT}`));
