ee70fa1fc59a513269f3fa3790b39736d8e19651
m-rejdych <rejdych.m.r@gmail.com>
2024-11-04 01:52:32 +0100

    Added ls command.

--- src/index.js
+++ src/index.js
@@ -8,25 +8,31 @@ const main = async () => {
     throw new Error('"template" arg is required.');
   }
 
-  const templatePath = path.join(__dirname, '..', 'templates', template);
-  if (!(await exists(templatePath))) {
-    throw new Error('Template does not exist.');
-  }
+  const templatesPath = path.join(__dirname, '..', 'templates');
 
-
-  if ((await lstat(templatePath)).isDirectory()) {
-    const filesToCopy = await readdir(templatePath);
-    await Promise.all(
-      filesToCopy
-        .filter((filename) => !['node_modules', 'package-lock.json'].includes(filename))
-        .map(async (filename) => {
-          const filePath = path.join(templatePath, filename);
-          const destPath = path.join(process.cwd(), filename);
-          await copy(filePath, destPath);
-        }),
-    );
+  if (template === 'ls') {
+    const templates = await readdir(templatesPath);
+    console.log(templates.join('\n'));
   } else {
-    await copyFile(templatePath, path.join(process.cwd(), template));
+    const templatePath = path.join(templatesPath, template);
+    if (!(await exists(templatePath))) {
+      throw new Error('Template does not exist.');
+    }
+
+    if ((await lstat(templatePath)).isDirectory()) {
+      const filesToCopy = await readdir(templatePath);
+      await Promise.all(
+        filesToCopy
+          .filter((filename) => !['node_modules', 'package-lock.json'].includes(filename))
+          .map(async (filename) => {
+            const filePath = path.join(templatePath, filename);
+            const destPath = path.join(process.cwd(), filename);
+            await copy(filePath, destPath);
+          }),
+      );
+    } else {
+      await copyFile(templatePath, path.join(process.cwd(), template));
+    }
   }
 };
 
